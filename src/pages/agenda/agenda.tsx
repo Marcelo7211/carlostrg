b2c0dcef2d0c2417d8bf8466c1182af64064fbbc
Marcelo Barboza <marcelo.barboza.cmkp@gmail.com>
2024-09-22 13:27:29 -0300

    ajuste no texto

--- src/pages/agenda/agenda.tsx
+++ src/pages/agenda/agenda.tsx
@@ -35,7 +35,7 @@ export default function Agenda(){
                             href="https://dojopaulista.actuar.com"
                             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                            Inicie conosco
+                            Saiba mais
                         </a>
                         <a href="https://dojopaulista.actuar.com/fale-conoscov2" className="text-sm font-semibold leading-6 text-gray-500"> {/* Alterado para texto mais claro */}
                             Sobre nós <span aria-hidden="true">→</span>
