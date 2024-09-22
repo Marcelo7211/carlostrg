24d89b06fc7eab60af9f5acd64d246870b4c4947
Marcelo Barboza <marcelo.barboza.cmkp@gmail.com>
2024-09-22 01:24:59 -0300

    first commit

--- src/pages/about/sobre.tsx
+++ src/pages/about/sobre.tsx
@@ -15,13 +15,6 @@ export default function Sobre() {
                         className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                     />
                 </div>
-                <div className="mx-auto max-w-2xl py-10 sm:py-10 lg:py-10">
-                    <div className="text-center">
-                        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                            Sobre mim
-                        </h1>
-                    </div>
-                </div>
                 <SobreComponent />
             </div>
         );
